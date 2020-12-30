929dcde11e229540c37578606faaebebedf88060
nnnvuuu <zhixin.chen72@gmail.com>
2020-12-29 20:34:20 -0500

    updated overflow

--- client/src/components/HomePage/HomeBody/HomeBody.js
+++ client/src/components/HomePage/HomeBody/HomeBody.js
@@ -5,9 +5,9 @@ function HomeBody(props) {
     <div>
         {/* <h1 className="home box"></h1> */}
         <div className="section-1 "></div>
-        <div className="section-2 ">
+        {/* <div className="section-2 ">
         hello;
-        </div>
+        </div> */}
     </div>
   );
 }
