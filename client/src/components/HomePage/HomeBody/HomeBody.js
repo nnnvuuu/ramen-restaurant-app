af0606c40b7810410efddba51cc55a30b419be3e
nnnvuuu <zhixin.chen72@gmail.com>
2020-12-30 11:14:40 -0500

    testing background-attachment:fixed doesn't work for iOS

--- client/src/components/HomePage/HomeBody/HomeBody.js
+++ client/src/components/HomePage/HomeBody/HomeBody.js
@@ -2,13 +2,10 @@ import React from 'react';
 import './HomeBody.css';
 function HomeBody(props) {
   return (
-    <div>
-        {/* <h1 className="home box"></h1> */}
+ 
         <div className="section-1 "></div>
-        {/* <div className="section-2 ">
-        hello;
-        </div> */}
-    </div>
+       
+
   );
 }
 
--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
   return (
     <div>
       <Navbar/>
-      <HomeBody/>
+      {/* <HomeBody/> */}
     </div>
   );
 };
