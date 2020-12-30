ca44cf6019b052b9e4f7a5d57b739d23405766d9
nnnvuuu <zhixin.chen72@gmail.com>
2020-12-29 19:47:53 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ connection.once('open', ()=>{
   app.use(express.static("client/build"));
   
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
     });
 
     
