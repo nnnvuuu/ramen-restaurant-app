a0e9ab7580ef8ed06f2c31363d3c20ee2c7396a7
nnnvuuu <zhixin.chen72@gmail.com>
2020-12-31 12:56:36 -0500

    updated sticky

--- client/src/components/HomePage/HomeBody/HomeBody.js
+++ client/src/components/HomePage/HomeBody/HomeBody.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './HomeBody.css';
 function HomeBody(props) {
   return (
- 
+    <>
         <div className="section-1 "></div>
-       
+        <div className="section-2 box "></div>
+
+
+    </>
 
   );
 }
