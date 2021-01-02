fbffa30be232e98fe26628fe187c5f51eb366c7c
nnnvuuu <zhixin.chen72@gmail.com>
2021-01-01 20:16:56 -0500

    updated workflow

--- client/src/components/HomePage/HomeBody/HomeBody.js
+++ client/src/components/HomePage/HomeBody/HomeBody.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './HomeBody.css';
+import RamenTitlePng from '../../../images/RAMEN-title.png';
+
 function HomeBody(props) {
   return (
     <>
-        <div className="section-1 "></div>
-        <div className="section-2 box "></div>
+      
 
+        <div className="section-1 ">
+          <img src={RamenTitlePng}/>
+        </div>
+        <div className="section-2 box ">about</div>
+        <div className="section-3 box ">menu</div>
+        <div className="section-4 box ">gallery</div>
+        <div className="section-5 box ">address</div>
+        <div className="section-6 box ">map</div>
+        <div className="section-7 box ">footer</div>
 
     </>
 
