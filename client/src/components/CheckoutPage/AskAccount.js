595fd1aa7da90f60bad5287492223fb72624a270
Zhixin chen <zhixin.chen72@gmail.com>
2021-01-20 23:49:21 -0500

    zz

--- client/src/components/CheckoutPage/AskAccount.js
+++ client/src/components/CheckoutPage/AskAccount.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import {Box, Grid,makeStyles, Typography,Button,Divider} from '@material-ui/core';
+import {Box, Grid,makeStyles, Typography,
+    Button,Divider,TextField,FormControl,
+    InputLabel,FormHelperText,Input,FormControlLabel,
+    Checkbox,
+ } from '@material-ui/core';
+ import {Link} from 'react-router-dom';
 
 const useStyle = makeStyles(()=>({
     root:{
-      marginTop:'13rem',
+      marginTop:'10rem',
     },
-    button:{
+    guestButton:{
 
         paddingLeft:'50px',
         paddingRight:'50px',
         backgroundColor:'#000000',
         color:'#ffffff',
         marginTop:'25px',
-        
+        textTransform:'none',
+        borderRadius:7,
         '&:hover':{
             paddingLeft:'50px',
             paddingRight:'50px',
             backgroundColor:'#000000',
             color:'#ffffff',
+            textTransform:'none',
+            borderRadius:7,
         }
+    },
+    customerButton:{
+        paddingLeft:'45px',
+        paddingRight:'45px',
+        backgroundColor:'#000000',
+        color:'#ffffff',
+        marginTop:'25px',
+        textTransform:'none',
+        borderRadius:7,
+        '&:hover':{
+            paddingLeft:'45px',
+            paddingRight:'45px',
+            backgroundColor:'#000000',
+            color:'#ffffff',
+            marginTop:'25px',
+            textTransform:'none',
+            borderRadius:7,
+        }
+    },
+    midDivider:{
+        padding:'0.4px',
+        backgroundColor:'#000000'
+    },
+    // guestDivider:{
+    //     backgroundColor:'#000000',
+        
+    // }
+    link:{
+        textDecoration:'none',
     }
 }))
 
@@ -28,15 +65,56 @@ const AskAccount = () => {
     return ( 
         <div className={classes.root}>
             <Grid container justify='center' >
-                <Grid item lg={5}>
+                <Grid item lg={3}>
                     <Typography variant='h4'>
                         <Box>
                             Returning Customers
                         </Box>
                     </Typography>
+                  <FormControl filled fullWidth margin='dense'>
+                    <InputLabel htmlFor="my-input">Email address</InputLabel>
+                    <Input id="my-input" aria-describedby="my-helper-text"   />
+                    <FormHelperText id="my-helper-text">
+                        We'll never share your email.
+                    </FormHelperText>
+                 </FormControl>
+                
+                <FormControl filled fullWidth margin='dense'>
+                  <InputLabel htmlFor="my-input">Password</InputLabel>
+                  <Input id="my-input" aria-describedby="my-helper-text"   />
+                </FormControl>
+
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                    //   checked={state.checkedF}
+                    //   onChange={handleChange}
+                    //   name="checkedF"
+                     color="#000000"
+                    />
+                  }
+                  label="Keep me logged in"
+                />
+                <Box>
+                  <Button className={classes.customerButton}>
+                    <Typography>
+                        <Box fontSize={15}>
+                            Log in
+                        </Box>
+                    </Typography>
+                  </Button>
+                </Box>
+
+               <Box mt={3}>
+                <Link className={classes.link}>
+                Forgot your email address/password?
+                </Link>
+                </Box>
+
                 </Grid>
-                <Grid item lg={2}>
-                 <Divider orientation='vertical' />
+                <Grid  lg={1}/>
+                <Grid item lg={1}>
+                 <Divider orientation='vertical'  className={classes.midDivider}/>
                 </Grid>
                 <Grid item lg={3}   >
                        <Typography variant='h4'>
@@ -49,13 +127,19 @@ const AskAccount = () => {
                                 Checkout without logging in. You can always create an account later.
                             </Box>
                        </Typography>
-                       <Button className={classes.button}>
+                       <Button className={classes.guestButton}>
                          <Typography variant='h6'>
-                              <Box fontSize={15}>
+                              <Box fontSize={20}>
                                  Continue
                               </Box>
                          </Typography>
                        </Button>
+                       <Box mt={6}>
+                           <Divider/>
+                       </Box>
+                       <Box mt={3}>
+                           Do you want to <Link className={classes.link}>create an account</Link> now?
+                       </Box>
                 </Grid>
             </Grid>
         </div>
